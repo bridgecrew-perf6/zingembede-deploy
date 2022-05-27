f16233816ac16a01137819e5f70a5097438a1abb
hip06 <khongpikten21@gmail.com>
2022-05-27 18:03:02 +0700

    8

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import "./App.scss"
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
 import { connect } from "react-redux"
 import { Route, BrowserRouter, Switch } from "react-router-dom"
 import "sweetalert2/src/sweetalert2.scss"
@@ -37,37 +37,46 @@ class App extends Component {
 
     render() {
         return (
-            <BrowserRouter>
-                <Scrollbars
-                    onScroll={this.handleOnScroll}
-                    style={{ width: "100%", height: "100vh" }}
-                >
-                    <div className="search"> <Search /></div>
-                    <div className="content-main">
-                        <div className="left-sidebar">
-                            <LeftSidebar />
-                        </div>
-                        {!this.props.modal && <div className="right-sidebar">
-                            <RightSideBar />
-                        </div>}
-                        <Switch>
-                            <Route exact path="/"><UseisrAuthenticated /></Route>
-                            <Route path="/login"><Login /></Route>
-                            <Route path="/home"><Home /></Route>
-                            <Route path="/detail-album/:id"><DetailAlbum /></Route>
-                            <Route path="/detail-chart"><DetailChart /></Route>
-                            <Route path="/detail-week-top/:country"><DetailWeekTop /></Route>
-                            <Route path="/detail-singer/:alias"><DetailSinger /></Route>
-                            <Route path="/detail-search/:keyword"><DetailSearch /></Route>
-                            <Route path="/personal"><Personal /></Route>
-                            <Route path="/new-release"><NewRelease /></Route>
-                            <Route path="/detail-top-100"><DetailTop100 /></Route>
-                            <Route path="/detail-recent"><DetailRecent /></Route>
-                            <Route path="/detail-playlist/:id"><Playlist /></Route>
-                        </Switch>
-                    </div>
-                </Scrollbars>
-            </BrowserRouter>
+            <Fragment>
+                <div className="main">
+                    <BrowserRouter>
+                        <Scrollbars
+                            onScroll={this.handleOnScroll}
+                            style={{ width: "100%", height: "100vh" }}
+                        >
+                            <div className="search"> <Search /></div>
+                            <div className="content-main">
+                                <div className="left-sidebar">
+                                    <LeftSidebar />
+                                </div>
+                                {!this.props.modal && <div className="right-sidebar">
+                                    <RightSideBar />
+                                </div>}
+                                <Switch>
+                                    <Route exact path="/"><UseisrAuthenticated /></Route>
+                                    <Route path="/login"><Login /></Route>
+                                    <Route path="/home"><Home /></Route>
+                                    <Route path="/detail-album/:id"><DetailAlbum /></Route>
+                                    <Route path="/detail-chart"><DetailChart /></Route>
+                                    <Route path="/detail-week-top/:country"><DetailWeekTop /></Route>
+                                    <Route path="/detail-singer/:alias"><DetailSinger /></Route>
+                                    <Route path="/detail-search/:keyword"><DetailSearch /></Route>
+                                    <Route path="/personal"><Personal /></Route>
+                                    <Route path="/new-release"><NewRelease /></Route>
+                                    <Route path="/detail-top-100"><DetailTop100 /></Route>
+                                    <Route path="/detail-recent"><DetailRecent /></Route>
+                                    <Route path="/detail-playlist/:id"><Playlist /></Route>
+                                </Switch>
+                            </div>
+                        </Scrollbars>
+                    </BrowserRouter>
+                </div>
+                <div className="no-support">
+                    <i className="fa-solid fa-circle-info"></i>
+                    <div>Không hỗ trợ màn hình nhỏ hơn 900px</div>
+                    <div>Ae mở bằng laptop hoặc tablet giùm :v</div>
+                </div>
+            </Fragment>
         )
     }
 }
